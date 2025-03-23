ee0bbc0af9a52a252d3a6424c241e5f9ff3a4a6c
Ilya Zoria <ilzorya@gmail.com>
2025-03-23 19:36:02 +0100

    added analytics 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "../components/theme-provider"
+import Script from 'next/script'
 import "./globals.css"
 
 export const metadata = {
@@ -15,6 +16,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet" />
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="078490c7-d605-4a05-a992-e25f51bb189f"
+          strategy="afterInteractive"
+        />
       </head>
       <body>
         <ThemeProvider defaultTheme="light">
