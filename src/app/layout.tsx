3d32426065d0ed03f024c6ed2e96d67b32277565
Ilya Zoria <ilzorya@gmail.com>
2025-03-23 20:33:05 +0100

    thumbnail + favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import Script from 'next/script'
 import "./globals.css"
 
 export const metadata = {
-  title: 'Ilya Zoria Portfolio',
-  description: 'Portfolio website of Ilya Zoria, UI/UX and product designer',
+  title: 'Ilya Zoria',
+  description: 'Product designer',
 }
 
 export default function RootLayout({
@@ -15,7 +15,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <meta property="og:title" content="Ilya Zoria" />
+        <meta property="og:description" content="Product designer" />
+        <meta property="og:image" content="/images/thumbnail.jpg" />
+        <meta property="og:url" content="https://ilyazoria.design/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Ilya Zoria" />
+        <meta name="twitter:description" content="Product designer" />
+        <meta name="twitter:image" content="/thumbnail.jpg" />
         <Script
           src="https://cloud.umami.is/script.js"
           data-website-id="078490c7-d605-4a05-a992-e25f51bb189f"
