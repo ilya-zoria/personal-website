39391375d4841720838d86b6d07c38b08ab1e270
Ilya Zoria <ilzorya@gmail.com>
2025-04-16 20:46:01 +0200

    ...

--- src/components/craft-card.tsx
+++ src/components/craft-card.tsx
@@ -26,13 +26,13 @@ export function CraftCard() {
     { src: "/Craft/back-to-future.mov", alt: "back-to-future" },
     // { src: "/Craft/intertactive-widgets.mp4", alt: "intertactive-widgets" },
     { src: "/Craft/ai_learning_companion_interactions.mp4", alt: "learning_companion_interactions" },
+    { src: "/Craft/triple_tree.mp4", alt: "triple_tree" },
     { src: "/Craft/nivia.mp4", alt: "nivia" },
   ];
   const craftItems2 = [
     { src: "/Craft/spichify.mp4", alt: "spichify" },
     { src: "/Craft/carerix-job-preview.mp4", alt: "Carerix-job-preview" },
     { src: "/Craft/carerix_vision.mp4", alt: "carerix_vision" },
-    { src: "/Craft/triple_tree.mp4", alt: "triple_tree" },
   ];
 
   return (
